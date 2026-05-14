7268c771b48b21d042d2c06d63942121ca66b4e3
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2026-05-14 12:20:01 -0700

    fix: predeployment script

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import 'dotenv';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaClient } from '@prisma/client/index';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import { Resend } from 'resend';
