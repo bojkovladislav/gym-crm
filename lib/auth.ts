da5a11326875feb065fc5ea207f05b5b5763d6c5
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2026-05-14 12:57:20 -0700

    fix: auth.ts connection

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,18 +1,10 @@
 import 'dotenv';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 import { Resend } from 'resend';
+import prisma from './prisma';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({ adapter });
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
